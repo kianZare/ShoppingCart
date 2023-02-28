09512709ceddab050c196a62f746e6797ceed377
Kian Zareinajad <kzareinajad@gmail.com>
2023-02-28 18:07:43 +0000

    added products

---
+++ products.js
@@ -0,0 +1,57 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "Queen panel bed",
+        price: 999.99,
+        imageUrl: "./images/product-1.jpeg"
+    },
+    {
+        id: 2,
+        title: "King size panel bed",
+        price: 699.99,
+        imageUrl: "./images/product-2.jpeg"
+    },
+    {
+        id: 3,
+        title: "Queen panel bed",
+        price: 499.99,
+        imageUrl: "./images/product-3.jpeg"
+    },
+    {
+        id: 4,
+        title: "Queen panel bed",
+        price: 1199.99,
+        imageUrl: "./images/product-4.jpeg"
+    },
+    {
+        id: 5,
+        title: "Queen panel bed",
+        price: 399.99,
+        imageUrl: "./images/product-5.jpeg"
+    },
+    {
+        id: 6,
+        title: "Queen panel bed",
+        price: 799.99,
+        imageUrl: "./images/product-6.jpeg"
+    },
+    {
+        id: 7,
+        title: "Queen panel bed",
+        price: 1499.99,
+        imageUrl: "./images/product-7.jpeg"
+    },
+    {
+        id: 8,
+        title: "Queen panel bed",
+        price: 299.99,
+        imageUrl: "./images/product-8.jpeg"
+    },
+    {
+        id: 9,
+        title: "Queen panel bed",
+        price: 399.99,
+        imageUrl: "./images/product-9.jpeg"
+    },
+
+]
